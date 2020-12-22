65614fed73a0ddeb65b26b3e353cb5869555fbf0
Alan Tran <contactalantran@gmail.com>
2020-12-22 16:24:37 -0500

    added checkout page, checkout item component and updated cart to use selectors

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,27 +1,45 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { createStructuredSelector } from 'reselect';
+import { withRouter } from 'react-router-dom'; //access to location, match, and history props from Router in App.js
 
 import CustomButton from '../custom-button/custom-button.component';
 import CartItem from '../cart-item/cart-item.component';
 import { selectCartItems } from '../../redux/cart/cart.selectors';
+import { toggleCartHidden } from '../../redux/cart/cart.actions';
 
 import './cart-dropdown.styles.scss';
 
-const CartDropdown = ({ cartItems }) => {
+const CartDropdown = ({ cartItems, history, dispatch }) => {
   return (
     <div className='cart-dropdown'>
       <div className='cart-items'>
-        {cartItems.map((cartItem) => (
-          <CartItem key={cartItem.id} item={cartItem} />
-        ))}
+        {cartItems.length ? (
+          cartItems.map((cartItem) => (
+            <CartItem key={cartItem.id} item={cartItem} />
+          ))
+        ) : (
+          <span className='empty-message'>Your cart is empty</span>
+        )}
       </div>
-      <CustomButton>GO TO CHECKOUT</CustomButton>
+      <CustomButton
+        onClick={() => {
+          history.push('/checkout');
+          dispatch(toggleCartHidden());
+        }}
+      >
+        GO TO CHECKOUT
+      </CustomButton>
     </div>
   );
 };
 
-const mapStateToProps = (state) => ({
-  cartItems: selectCartItems(state),
+const mapStateToProps = createStructuredSelector({
+  //even though it's one thing, using this will make things more scalable
+  cartItems: selectCartItems, //invoking the selector for memoization
 });
 
-export default connect(mapStateToProps)(CartDropdown);
+export default withRouter(connect(mapStateToProps)(CartDropdown));
+//HOC take in components and spit them out
+//order of nesting matters
+//if mapDispatchToProps is not in the params, connect will pass it as a prop in the component
--- src/redux/cart/cart.selectors.js
+++ src/redux/cart/cart.selectors.js
@@ -1,18 +1,34 @@
 import { createSelector } from 'reselect';
 
-const selectCart = (state) => state.cart;
+const selectCart = (state) => state.cart; //input selector
 
+//output selector
+//createSelector turns this into a memoized selector
 export const selectCartItems = createSelector(
+  [selectCart], //references an array of input selectors
+  (cart) => cart.cartItems //function that returns the value we want out of this selector.
+  //params are in the order that are called in the array
+);
+
+export const selectCartHidden = createSelector(
   [selectCart],
-  (cart) => cart.cartItems
+  (cart) => cart.hidden
 );
 
 export const selectCartItemsCount = createSelector(
-  [selectCartItems],
+  [selectCartItems], //doesn't have to be input selectors referenced here
   (cartItems) =>
     cartItems.reduce(
       (accumulatedQuantity, cartItem) =>
         accumulatedQuantity + cartItem.quantity,
       0
     )
+);
+
+export const selectCartTotal = createSelector([selectCartItems], (cartItems) =>
+  cartItems.reduce(
+    (accumulatedQuantity, cartItem) =>
+      accumulatedQuantity + cartItem.quantity * cartItem.price,
+    0
+  )
 );
