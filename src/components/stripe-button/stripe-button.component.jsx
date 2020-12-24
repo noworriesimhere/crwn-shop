d5abfc0d2d37b29f02d627aa9bf7c173bc05f331
Alan Tran <contactalantran@gmail.com>
2020-12-24 09:45:10 -0500

    updating stripe integration and updating our favicon and title

---
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import StripeCheckout from 'react-stripe-checkout';
+
+const StripeButton = ({ price }) => {
+  const priceForStripe = price * 100;
+  const publishableKey =
+    'pk_test_51Hp1ZEBh0eXPB1DUIJd924P3LobZNKdnorgzaBraWC5MfsBtb6qhAWpjUk0aDvKf9QK3F2EAL6BgZTSrL3LZY75e003VUIOmwn';
+
+  const onToken = (token) => {
+    console.log(token);
+    alert('Payment Successful');
+  };
+
+  return (
+    <StripeCheckout
+      label='Pay Now'
+      name='Pinto Clothing Ltd.'
+      billingAddress
+      shippingAddress
+      image='https://svgshare.com/i/CUz.svg'
+      description={`Your total is $${price}`}
+      amount={priceForStripe}
+      panelLbael='Pay Now'
+      token={onToken}
+      stripeKey={publishableKey}
+    />
+  );
+};
+
+export default StripeButton;
